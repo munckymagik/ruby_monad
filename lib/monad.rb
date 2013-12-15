90467dc705df3d0d3618ea2fb842fac02532e3fe
Dan Munckton <dangit@munckfish.net>
2013-12-15 22:50:12 +0000

    Separate the Failable class and spec out into separate files

--- lib/monad.rb
+++ lib/monad.rb
@@ -14,42 +14,6 @@ module Monad
       bind(block)
     end
   end
-
-  class Failable < Monad
-    attr_reader :value, :is_success
-    alias :success? :is_success
-
-    def initialize(value, is_success)
-      @value = value
-      @is_success = is_success
-    end
-
-    def bind(bindee)
-      if @is_success
-        bindee.call(@value)
-      else
-        self
-      end
-    end
-
-    def self.unit(val)
-      self.new(val, true)
-    end
-
-    def to_s
-      if @is_success
-        "Success(#{@value})"
-      else
-        "Failure(#{@value})"
-      end
-    end
-  end
-
-  def self.success(value)
-    Failable.new value, true
-  end
-
-  def self.failure(value)
-    Failable.new value, false
-  end
 end
+
+require 'monad/failable'
